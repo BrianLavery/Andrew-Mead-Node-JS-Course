c0abd167d1ad0bef4621fb592e5157d9d1b5e3bd
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-09-11 19:47:39 +0200

    up to lecture 59

--- 00-playground/56-default-params.js
+++ 00-playground/56-default-params.js
@@ -4,3 +4,20 @@ const greeter = (name = 'user') => {
 
 greeter('Andrew')
 greeter()
+
+// We always want default parameters when we destructure in a function
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined
+}
+
+product2 = undefined
+
+const transaction = (type, { label, stock } = {}) => {
+  console.log(type, label, stock)
+}
+
+transaction('order', product) // works as product is an object
+transaction('order', product2) // Crashes as it is undefined except we have the default params
