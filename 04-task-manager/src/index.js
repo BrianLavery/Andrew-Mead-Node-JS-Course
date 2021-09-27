70e8e225726dcd024be4f5a31c53f7f74c21745d
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-09-27 10:04:27 +0100

    minor changes

--- 04-task-manager/src/index.js
+++ 04-task-manager/src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // This one line automatically parses incoming JSON to an object (req.body)
 app.use(express.json())
